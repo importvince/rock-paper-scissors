b1813f5467a996d595dd5c79bcd99cdaca5c1bd6
Vinny 2 Thumbs <97123672+importvince@users.noreply.github.com>
2023-02-20 14:59:05 -0600

    Basic scoreboard functionaility complete

--- game-v3.js
+++ game-v3.js
@@ -1,9 +1,12 @@
-// create variables for buttons
+// create variables for buttons, result, and scorebaord
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const results = document.querySelector('.results-container');
-const score = document.querySelector('.scoreboard')
+const playerScore = document.querySelector('#player');
+const computerScore = document.querySelector('#computer');
+const tieScore = document.querySelector('#tie');
+
 
 const buttons = document.querySelectorAll('button');
 
@@ -13,21 +16,31 @@ buttons.forEach((button) => {
 
 //let computerPlay = getComputerChoice();
 
+let playerTally = 0;
+let computerTally = 0;
+let tieTally = 0;
+
 function playRound() {
     let player = this.id;
     let computer = getComputerChoice();
 
     if (player == computer) {
         results.textContent = 'tie';
+        tieTally += 1;
+        tieScore.textContent = tieTally;
         //return "tie";
     }
     else if ((player == 'rock' && computer == 'paper') || (player == 'paper' && computer == 'scissors') || (player == 'scissors' && computer == 'rock')) {
         results.textContent = 'computer wins again...'
+        computerTally += 1;
+        computerScore.textContent = computerTally;
        
         // return `Computer wins again...`;
     }
     else if((player == 'rock' && computer == 'scissors') || (player == 'scissors' && computer == 'paper') || (player == 'paper' && computer =='rock')) {
         results.textContent = 'You win!'
+        playerTally += 1;
+        playerScore.textContent = playerTally;
         //return `You win!`;
     }
 }
